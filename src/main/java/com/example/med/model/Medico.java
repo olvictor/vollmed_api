1c035f3b3389e134dd45b73f475dd2a0bc75fd18
Victor olv <victorolvdev@gmail.com>
2024-06-17 22:29:29 -0300

    criacao do modelo de medico/endereco

---
+++ src/main/java/com/example/med/model/Medico.java
@@ -0,0 +1,20 @@
+package com.example.med.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medicos")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
